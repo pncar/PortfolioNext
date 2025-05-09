3a3ff359acf7f94ae0557721ac546dca81376c63
Pablo <pncar94@gmail.com>
2025-05-09 20:01:54 -0300

    Switched back to v3 of tailwind

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -18,10 +18,10 @@ const Navbar = () => {
   }
     
   return(
-  <div className="flex md:p-6 bg-primary-950 md:bg-transparent shadow-lg md:shadow-none border-b md:border-b-0 border-primary-600/50">
+  <div className="flex md:p-6 bg-primary-950 md:bg-transparent shadow-lg md:shadow-none border-b md:border-b-0 border-primary-600 border-opacity-50">
       <div className="flex flex-row md:flex-col gap-2 w-full py-2 md:py-0 px-2">
         <h3 className="hidden md:block text-sm py-4">Categories</h3>
-        <ul className="space-y-2 font-medium w-full md:w-auto flex flex-row md:flex-col">
+        <ul className="gap-y-2 font-medium w-full md:w-auto flex flex-row md:flex-col">
           <LinkItem to="/"><FaUser/><span className="hidden md:flex">About</span></LinkItem>
           <LinkItem to="/skills"><FaStar/><span className="hidden md:flex">Skills</span></LinkItem>
           <LinkItem to="/experience"><FaChessKnight/><span className="hidden md:flex">Experience</span></LinkItem>
@@ -29,7 +29,7 @@ const Navbar = () => {
           <LinkItem to="/blog"><FaBlog/><span className="hidden md:flex">Blog</span></LinkItem>
         </ul>
         <h3 className="hidden md:block text-sm py-4">Links</h3>
-        <ul className="space-y-2 font-medium w-full md:w-auto flex flex-row md:flex-col justify-end md:justify-start">
+        <ul className="gap-y-2 font-medium w-full md:w-auto flex flex-row md:flex-col justify-end md:justify-start">
           <LinkItem to={`https://github.com/pncar`}><FaGithub/><span className="hidden md:flex">Github</span></LinkItem>
           <LinkItem to={`https://www.linkedin.com/in/pncar`}><FaLinkedin/><span className="hidden md:flex">Linkedin</span></LinkItem>
           <LinkItem to={`https://wa.me/541169698665`}><FaWhatsapp/><span className="hidden md:flex">Whatsapp</span></LinkItem>
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+module.exports = {
+  content:[
+	  "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+	  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+	  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+		colors: {
+			primary: colors.zinc,
+			brand: colors.blue
+		},
+        fontFamily: {
+			sans: ['var(--font-geist)'],
+			archivo: ['var(--font-archivo)'],
+			mono: ['var(--font-geist-mono)']
+        },
+	},
+  },
+  plugins: [],
+}
+
